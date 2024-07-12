e6a90ad4c54e9b912ef332e705c76b69f1981712
Cristian Sansão <169001529+SansaoYago@users.noreply.github.com>
2024-07-11 22:45:50 -0300

    Atualizando estilo em mobile first

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-function clickMenu() {
-    if (itens.style.display == 'block') {
-        itens.style.display = 'none'
-    } else {
-        itens.style.display = 'block'
-    }
-}
+
 
 /*function mudouTamanho() {
     if (window.innerWidth >= 668) {
